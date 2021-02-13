35fd62bfd64e69416e4d45d226b585cb610d56d8
Ahmed <abeebahmed@gmail.com>
2021-02-13 17:20:26 +0100

    bug fix

---
+++ frontend/build/precache-manifest.b5ee0b4919f5f78aa31ecc5503660a09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2859a73743daa985ebe73f755cc4471",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d53d5f3c08dc794d2b48",
+    "url": "/static/css/main.4e4d44a1.chunk.css"
+  },
+  {
+    "revision": "e2a4342a041c704f7e11",
+    "url": "/static/js/2.6ce702a2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6ce702a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d53d5f3c08dc794d2b48",
+    "url": "/static/js/main.9439a024.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
